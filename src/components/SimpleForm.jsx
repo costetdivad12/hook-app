ae33a7e2b3c73fcb9e337522405feb45e01a91ca
costetdivad12 <david.costet@iebem.edu.mx>
2022-06-08 16:44:55 -0500

    agregando use effect en el componente message

--- src/components/SimpleForm.jsx
+++ src/components/SimpleForm.jsx
@@ -1,17 +1,63 @@
-import React, { useEffect } from 'react'
-
+import React, { useEffect, useState } from "react";
+import "./simpleForm.css";
+import Message from "./Message";
 const SimpleForm = () => {
+  const [forms, setforms] = useState({
+    name: "",
+    email: "",
+  });
 
-useEffect(()=>{
-console.log('hey');
-})
+  const { name, email } = forms;
 
+  useEffect(() => {
+    console.log("hey");
+  }, []);
+
+  useEffect(() => {
+    console.log("FORM");
+  }, [forms]);
+
+  useEffect(() => {
+    console.log("FORM");
+  }, [email]);
+
+  const handleChangeName = ({ target }) => {
+    console.log(target);
+
+    setforms({
+      ...forms,
+      [target.name]: target.value,
+      [target.email]: target.value,
+    });
+  };
 
   return (
     <>
-    <h1>useEfect</h1>
-    </>
-  )
-}
+      <h1>useEfect</h1>
+      <div className="form-group">
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="tu nombre"
+          autoComplete="off"
+          value={name}
+          onChange={handleChangeName}
+        />
 
-export default SimpleForm
+        <input
+          type="text"
+          name="email"
+          className="form-control"
+          placeholder="abs@gmail.com"
+          autoComplete="off"
+          value={email}
+          onChange={handleChangeName}
+        />
+      </div>
+      {name==='123' && <Message/>}
+    </>
+  );
+};
+
+export default SimpleForm;
